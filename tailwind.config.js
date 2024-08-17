15937ae5103c645deb519e89c2f5b833300d86bb
nirmit152007 <nirmitjee@gmail.com>
2024-08-17 09:43:36 +0530

    base code

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: "class", // Enable dark mode
+  theme: {
+    extend: {
+      colors: {
+        "neon-orange": "#FF4500",
+        "neon-blue": "#00BFFF",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
